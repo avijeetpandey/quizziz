e71b339466b12dc547411669f5d2194bfae9fa32
avijeet <avijeetpandey25@gmail.com>
2024-08-01 20:36:20 +0530

    add: start screen

---
+++ lib/core/quizzez.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:quizziz/widgets/start_screen_widget.dart';
+
+class Quizzez extends StatelessWidget {
+  const Quizzez({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: StartScreenWidget(),
+    );
+  }
+}
