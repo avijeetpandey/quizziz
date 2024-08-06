8bba94c39d8acb1582874d6aa90a098a41723b54
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-07 00:14:43 +0530

    add: question button

--- lib/core/quiz_page.dart
+++ lib/core/quiz_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizziz/data/questions.dart';
 import 'package:quizziz/widgets/answer_button.dart';
 
 class QuizPage extends StatefulWidget {
@@ -9,23 +10,31 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  var currentQuestion = questions[3];
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Questions",
-            style: TextStyle(
-                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 30),
-          AnswerButton(answerText: "Text", onTap: () {}),
-          AnswerButton(answerText: "Text", onTap: () {}),
-          AnswerButton(answerText: "Text", onTap: () {}),
-          AnswerButton(answerText: "Text", onTap: () {})
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
